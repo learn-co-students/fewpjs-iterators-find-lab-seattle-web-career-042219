6008e73d2bfcb244bb563fd096c9560b2fd7875e
Danny Tseng <github email address>
2019-06-12 14:02:24 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let target = array.find(record => record.result == "W");
+  if (target) {
+    return target.year;
+  }
+  return undefined;
+}
