dd6a4b7fdfe03714d386fb40fdc73b91eb2d1923
jkshareef <jamalkshareef@gmail.com>
2019-06-13 14:57:50 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// function superbowlWin(array) {
+//     array.find(isWin)
+// }
+
+// function isWin(element, index, array) {
+//   if (element.result === "W") {
+//     return element.year
+//   } 
+// }
+
+
+
+function superbowlWin(array) {
+  let found = array.find( element => element.result === "W")
+  if (found) {
+    return found.year
+  } else {
+    return undefined
+  }
+}
